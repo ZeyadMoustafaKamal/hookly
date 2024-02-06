ba3b1f1fbe0ac29f9cdc669019aebc7aef21a7bf
ZeyadMoustafaKamal <zeyad20062018@gmail.com>
2024-02-06 13:16:57 +0200

    Add main files & customize the User model

---
+++ accounts/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+class UserManager(BaseUserManager):
+    
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("You have to pass an email while creating a user")
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        
+        if extra_fields.get("is_staff", False) != True:
+            raise ValueError("You have to mark is_staff as True while creating a superuser")
+        if extra_fields.get("is_superuser", False) != True:
+            raise ValueError("You have to mark is_superuser as True while creating a superuser")
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(blank=False, unique=True)
+
+    objects = UserManager()
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
