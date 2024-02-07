63a3cc046dc4acc356580664d1fe32e7d066fc69
ZeyadMoustafaKamal <zeyad20062018@gmail.com>
2024-02-07 17:21:52 +0200

    style: add pre-commit and flake8 & change code style

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,10 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
 class UserManager(BaseUserManager):
-    
+
     def create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError("You have to pass an email while creating a user")
@@ -12,14 +12,14 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save()
         return user
-    
+
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
-        
-        if extra_fields.get("is_staff", False) != True:
+
+        if extra_fields.get("is_staff", False) is not True:
             raise ValueError("You have to mark is_staff as True while creating a superuser")
-        if extra_fields.get("is_superuser", False) != True:
+        if extra_fields.get("is_superuser", False) is not True:
             raise ValueError("You have to mark is_superuser as True while creating a superuser")
         return self.create_user(email, password, **extra_fields)
 
@@ -29,7 +29,6 @@ class User(AbstractUser):
     email = models.EmailField(_("email address"), blank=False, unique=True)
 
     objects = UserManager()
-    
+
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
-
